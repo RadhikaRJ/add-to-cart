e4e911deca14915fdd3cdddce934f81816709723
RadhikaRJ <radhika7002@gmail.com>
2021-02-23 18:26:38 +0530

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Cart from './Components/Cart';
+import Item from './Components/Item';
+import {useState} from "react";
 
 function App() {
+
+  const [totalItemCount, setTotalItemCount] = useState(0);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <Cart totalItemCount={totalItemCount}/>
+    <Item setTotalItemCount={setTotalItemCount} totalItemCount={totalItemCount}/>
+
+
     </div>
   );
 }
