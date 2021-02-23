ce036d2490ec7bc6d63e81cd59f4e89daf4556e5
RadhikaRJ <radhika7002@gmail.com>
2021-02-23 18:26:49 +0530

    Add: Cart Component

---
+++ src/Components/Cart.js
@@ -0,0 +1,13 @@
+
+
+function  Cart(props){
+
+    return(
+        <div>
+        <p>Cart total items: {props.totalItemCount}</p>
+        
+        
+        </div>
+    )
+}
+export default Cart;
